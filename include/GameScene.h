c225f87e4276df74b6948e57e7b409ac88ce8b1d
ArtemSerock <syrykha@internet.ru>
2026-03-17 23:05:01 +0500

    Documentate GameScene class

--- include/GameScene.h
+++ include/GameScene.h
@@ -2,7 +2,26 @@
 
 class GameScene : public IScene {
 public:
+  /**
+   * @brief Обработка входящих событий
+   * 
+   * @param ctx Игровой контекст
+   * @param event входящее событие
+   */
   void handleEvent(GameContext &ctx, const SDL_Event &event) override;
+
+  /**
+   * @brief Обработка вычислений текущей сцены
+   * 
+   * @param ctx игровой контекст
+   * @param deltaTime задержка времени между кадрами
+   */
   void update(GameContext &ctx, float deltaTime) override;
+
+  /**
+   * @brief Отрисовка текущей сцены
+   * 
+   * @param ctx игровой контекст
+   */
   void render(GameContext &ctx) const override;
 };
