5e180bacc23eaff9de341112a7fd84fb9bca9bf9
ArtemSerock <syrykha@internet.ru>
2026-03-17 22:54:10 +0500

    Add update method in GameContext class

--- include/GameContext.h
+++ include/GameContext.h
@@ -1,6 +1,8 @@
 #ifndef GAME_CONTEXT_H
 #define GAME_CONTEXT_H
+#include "IScene.h"
 #include <SDL3/SDL.h>
+#include <memory>
 
 enum class SceneType { NONE, MENU, GAME, GAME_OVER };
 
@@ -12,31 +14,38 @@ enum class SceneType { NONE, MENU, GAME, GAME_OVER };
  * и текущую сцену
  */
 struct GameContext {
-  SDL_Renderer *renderer = nullptr; //< Рендер игры
-  SDL_Window *window = nullptr;     //< Окно игры
-  int width = 800;              //< Длина экрана
-  int height = 600;             //< Ширина экрана
-  SceneType nextScene = SceneType::MENU;    //< Следующая сцена
-  SceneType currentScene = SceneType::NONE; //< текущая сцена
+  SDL_Renderer *renderer = nullptr;      //< Рендер игры
+  SDL_Window *window = nullptr;          //< Окно игры
+  int width = 800;                       //< Длина экрана
+  int height = 600;                      //< Ширина экрана
+  SceneType nextScene = SceneType::MENU; //< Следующая сцена
+
+  std::unique_ptr<IScene> activeScene = nullptr; //< Текущая сцена
 
   /**
-   * Конструкторы запрещают копирование, чтобы избежать двойного удаления указателей
-   * 
+   * Конструкторы запрещают копирование, чтобы избежать двойного удаления
+   * указателей
+   *
    */
   GameContext() = default;
-    GameContext(const GameContext&) = delete;
-    GameContext& operator=(const GameContext&) = delete;
+  GameContext(const GameContext &) = delete;
+  GameContext &operator=(const GameContext &) = delete;
 
-    /**
-     * @brief Декструктор, созданный для удаления указателей
-     * 
-     */
-    ~GameContext(){
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-    }
+  /**
+   * @brief Декструктор, созданный для удаления указателей
+   *
+   */
+  ~GameContext() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+  }
 
-
+  /**
+   * @brief Переключает на следующую сцену
+   *
+   * переключает текущую сцену на ту, что записана в nextScene
+   */
+  void updateScene();
 };
 
 #endif
---
+++ src/GameContext.cpp
@@ -0,0 +1,22 @@
+#include "GameContext.h"
+#include "GameScene.h"
+#include "MenuScene.h"
+#include <iostream>
+#include <memory>
+
+void GameContext::updateScene() {
+  if (nextScene == SceneType::NONE)
+    return;
+
+  switch (nextScene) {
+  case SceneType::MENU:
+    activeScene = std::make_unique<MenuScene>();
+    break;
+  case SceneType::GAME:
+    activeScene = std::make_unique<GameScene>();
+    break;
+  default:
+    break;
+  }
+  nextScene = SceneType::NONE;
+}
