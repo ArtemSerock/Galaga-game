bdff62d770574893cdbf6beb1ae24941d48ec44a
ArtemSerock <syrykha@internet.ru>
2026-03-17 23:07:22 +0500

    Documentate MenuScene class

--- include/MenuScene.h
+++ include/MenuScene.h
@@ -3,10 +3,36 @@
 #include "GameContext.h"
 #include "IScene.h"
 
+
+/**
+ * @brief Класс сцены главного меню
+ * 
+ * Здесь обрабатываются все входящие в эту сцену события
+ * и проводятся соответствующие вычисления
+ */
 class MenuScene : public IScene {
 public:
+  /**
+   * @brief Обработка входящих событий
+   * 
+   * @param ctx Игровой контекст
+   * @param event входящее событие
+   */
   void handleEvent(GameContext &ctx, const SDL_Event &event) override;
+
+  /**
+   * @brief Обработка вычислений текущей сцены
+   * 
+   * @param ctx игровой контекст
+   * @param deltaTime задержка времени между кадрами
+   */
   void update(GameContext &ctx, float deltaTime) override;
+
+  /**
+   * @brief Отрисовка текущей сцены
+   * 
+   * @param ctx игровой контекст
+   */
   void render(GameContext &ctx) const override;
 };
 #endif
