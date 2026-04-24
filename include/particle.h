4a3bcc88e464abaa548f55153d8d7f7fdc88b1f6
ArtemSerock <syrykha@internet.ru>
2026-04-24 23:09:34 +0500

    Add particle classes

---
+++ include/particle.h
@@ -0,0 +1,72 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include "GameContext.h"
+#include "particleConfig.h"
+#include <SDL3/SDL.h>
+
+/**
+ * @brief Базовый класс частиц
+ */
+class Particle {
+public:
+  /**
+   * @brief Конструктор класса
+   * @param tex Текстура
+   * @param x Координата по горизонтали
+   * @param y Координата по веритикали
+   * @param config Конфигурация из JSON файла
+   */
+  Particle(SDL_Texture *tex, float x, float y, const ParticleConfig &config);
+
+  /**
+   * @brief Деструктор
+   */
+  ~Particle();
+
+  Particle(const Particle &) = delete;
+  Particle operator=(const Particle &) = delete;
+
+  /**
+   * @brief Обновление логики
+   * @param dt Delta Time(Время в секундах с прошлого кадра)
+   */
+  virtual void update(float dt, const GameContext &ctx) = 0;
+
+  /**
+   * @brief Логика отрисовки
+   * @param renderer Рендер окна
+   */
+  virtual void draw(SDL_Renderer *renderer) const;
+
+  /**
+   * @brief Проверка на активность
+   * @return true, если частица активна, false -- иначе
+   */
+  bool isActive() const;
+
+  /**
+   * @brief Отключение частицы
+   */
+  void deactivate();
+
+  /**
+   * @brief Включение частицы
+   */
+  void activate();
+
+  /**
+   * @brief Получение координат частицы
+   * @return массив из x, y, weight, weight
+   */
+  const SDL_FRect &getRect() const;
+
+protected:
+  SDL_Texture *tex;    ///< Текстура частицы
+  SDL_FRect transform; ///< Позиция и размер
+  bool active = false; ///< Флаг для удаления из игрового цикла
+
+  float velocity_y; ///< Скорость по веритикали в пикселях в секунду
+  float velocity_x; ///< Скорость по горизонтали в пикселях в секунду
+};
+#endif // !PARTICLE_H
---
+++ include/particleConfig.h
@@ -0,0 +1,14 @@
+#ifndef PARTICLE_CONFIG_H
+#define PARTICLE_CONFIG_H
+
+/**
+ * @brief Конфигурация частиц
+ */
+struct ParticleConfig {
+  float vel_x;  ///< Скорость частицы по горизонтали
+  float vel_y;  ///< Скорость частицы по вертикали
+  float width;  ///< Длина частицы
+  float height; ///< Ширина частицы
+};
+
+#endif
