a9af1941448bb146cd7b0b21bc6db2a87411e8e3
ArtemSerock <syrykha@internet.ru>
2026-06-21 18:21:15 +0500

    Refactoring entity factory

--- include/entityFactory.h
+++ include/entityFactory.h
@@ -4,7 +4,7 @@
 #include "assetManager.h"
 #include "bee.h"
 #include "big_guy.h"
-#include "configManager.h" // 1. Подключаем наш менеджер конфигураций
+#include "configManager.h"
 #include "particleConfig.h"
 #include "player.h"
 #include "player_bullet.h"
@@ -13,6 +13,10 @@
 #include <nlohmann/json.hpp>
 #include <string>
 #include <type_traits>
+
+template <typename T>
+concept IsEnemyOrParticle =
+    std::is_base_of<Entity, T> || std::is_base_of<Particle, T>;
 
 /**
  * @brief Фабрика сущностей
@@ -35,9 +39,9 @@ public:
   /**
    * @brief создание объекта
    */
-  template <typename T>
-  std::unique_ptr<T> createEntity(const std::string &type, float x, float y,
-                                  SDL_Renderer *renderer) {
+  template <IsEnemyOrParticle T>
+  std::unique_ptr<T> createEntity(float x, float y, SDL_Renderer *renderer) {
+    constexpr std::string type = T::getType();
     if (!configData.contains(type)) {
       std::cerr << "FACTORY ERROR: Type '" << type
                 << "' not found in JSON config!" << std::endl;
@@ -49,34 +53,23 @@ public:
     std::string texPath = cfg.value("asset", "assets/placeholder.png");
     SDL_Texture *tex = manager.getTexture(texPath, renderer);
 
-    EntityConfig eConfig;
-    eConfig.hp = cfg.value("hp", 100.0f);
-    eConfig.speed = cfg.value("speed", 100.0f);
-    eConfig.width = cfg.value("width", 32.0f);
-    eConfig.height = cfg.value("height", 32.0f);
+    if constexpr (std::derived_from<T, Entity>) {
+      EntityConfig eConfig;
+      eConfig.hp = cfg.value("hp", 100.0f);
+      eConfig.speed = cfg.value("speed", 100.0f);
+      eConfig.width = cfg.value("width", 32.0f);
+      eConfig.height = cfg.value("height", 32.0f);
 
-    ParticleConfig pConfig;
-    pConfig.height = cfg.value("height", 25.0);
-    pConfig.width = cfg.value("width", 10.0);
-    pConfig.vel_x = cfg.value("vel_x", 0.0);
-    pConfig.vel_y = cfg.value("vel_y", -700.0);
+      return std::make_unique<T>(tex, x, y, eConfig);
 
-    if constexpr (std::is_same_v<T, Player>) {
-      if (type == "player") {
-        return std::make_unique<Player>(tex, x, y, eConfig);
-      }
-    } else if constexpr (std::is_same_v<T, PlayerBullet>) {
-      if (type == "player_bullet") {
-        return std::make_unique<PlayerBullet>(tex, x, y, pConfig);
-      }
-    } else if constexpr (std::is_same_v<T, Bee>) {
-      if (type == "bee") {
-        return std::make_unique<Bee>(tex, x, y, eConfig);
-      }
-    } else if constexpr (std::is_same_v<T, BigGuy>) {
-      if (type == "big_guy") {
-        return std::make_unique<BigGuy>(tex, x, y, eConfig);
-      }
+    } else if constexpr (std::derived_from<T, Particle>) {
+      ParticleConfig pConfig;
+      pConfig.height = cfg.value("height", 25.0);
+      pConfig.width = cfg.value("width", 10.0);
+      pConfig.vel_x = cfg.value("vel_x", 0.0);
+      pConfig.vel_y = cfg.value("vel_y", -700.0);
+
+      return std::make_unique<T>(tex, x, y, pConfig);
     }
 
     return nullptr;
